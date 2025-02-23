85d5e4e5cd7ecb215d50cd80324fbdc489d0f581
Ystaw <kristinkasvich@gmail.com>
2025-02-23 21:40:38 +0300

    feat: add interfaces for appLoader

--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -4,3 +4,8 @@ export interface ResponseData {
 export interface LoaderOptions {
     [key: string]: string | number | boolean;
 }
+
+export interface Env {
+    API_URL: string;
+    API_KEY: string;
+}
