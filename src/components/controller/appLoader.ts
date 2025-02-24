4b2643c4399a319704eea8e1b9f24499b8de8cc0
Ystaw <kristinkasvich@gmail.com>
2025-02-24 18:21:53 +0300

    feat: public for draw()

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -6,7 +6,7 @@ class AppLoader extends Loader {
         const env: Env = {
             API_URL: process.env.API_URL || '',
             API_KEY: process.env.API_KEY || '',
-        }
+        };
         super(env.API_URL, {
             apiKey: env.API_KEY,
         });
